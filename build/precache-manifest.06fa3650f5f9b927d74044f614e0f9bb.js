04db5babf32a62292dd186e05596c22abaeae38e
Alejandro de Frutos <adefrutoscasado@gmail.com>
2021-01-14 20:51:46 +0100

    Build

---
+++ build/precache-manifest.06fa3650f5f9b927d74044f614e0f9bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cceb4f949b8c5a8c95ff9b9d126a1b04",
+    "url": "/pampa-three/index.html"
+  },
+  {
+    "revision": "c527bd77be29a209e49b",
+    "url": "/pampa-three/static/css/main.fbd1a200.chunk.css"
+  },
+  {
+    "revision": "eb63b4e137ad004dbfd2",
+    "url": "/pampa-three/static/js/2.f738e7d4.chunk.js"
+  },
+  {
+    "revision": "c9b31f27ce0803bcbda04bfd0eba249e",
+    "url": "/pampa-three/static/js/2.f738e7d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c527bd77be29a209e49b",
+    "url": "/pampa-three/static/js/main.e628cd16.chunk.js"
+  },
+  {
+    "revision": "7617d3bc19e17f3d2275",
+    "url": "/pampa-three/static/js/runtime-main.88687fd5.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pampa-three/precache-manifest.c120e313d8a8642213012b8d7bde6f66.js"
+  "/pampa-three/precache-manifest.06fa3650f5f9b927d74044f614e0f9bb.js"
 );
 
 self.addEventListener('message', (event) => {
