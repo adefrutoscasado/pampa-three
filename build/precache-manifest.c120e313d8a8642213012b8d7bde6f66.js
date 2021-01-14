9c91f3474024f2d24754ac3f56da500148047a21
Alejandro de Frutos <adefrutoscasado@gmail.com>
2021-01-14 18:59:23 +0100

    Change title. Remove public file

---
+++ build/precache-manifest.c120e313d8a8642213012b8d7bde6f66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4334b02e8c31344d8dd3710987025013",
+    "url": "/pampa-three/index.html"
+  },
+  {
+    "revision": "302a26ab05ec8b7b6d33",
+    "url": "/pampa-three/static/css/main.fbd1a200.chunk.css"
+  },
+  {
+    "revision": "2bdbb0aa7bb1830e045c",
+    "url": "/pampa-three/static/js/2.1a3ba8c0.chunk.js"
+  },
+  {
+    "revision": "c9b31f27ce0803bcbda04bfd0eba249e",
+    "url": "/pampa-three/static/js/2.1a3ba8c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "302a26ab05ec8b7b6d33",
+    "url": "/pampa-three/static/js/main.158e0ffa.chunk.js"
+  },
+  {
+    "revision": "7617d3bc19e17f3d2275",
+    "url": "/pampa-three/static/js/runtime-main.88687fd5.js"
+  }
+]);
