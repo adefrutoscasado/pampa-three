b3026fcfe9066cecc3a27b4a5582c08f45fc4e51
Alejandro de Frutos <adefrutoscasado@gmail.com>
2021-01-14 17:18:25 +0100

    Github hosted model

---
+++ build/precache-manifest.fa4032fb8de81779f10bdbd6cccf3abe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68d36685f8374a218eecb64af25fe3fa",
+    "url": "/pampa-three/index.html"
+  },
+  {
+    "revision": "4b3403c9d39eaae7b7fe",
+    "url": "/pampa-three/static/css/main.c832bc5d.chunk.css"
+  },
+  {
+    "revision": "6db2dfe048a6fd377c95",
+    "url": "/pampa-three/static/js/2.b0c358bf.chunk.js"
+  },
+  {
+    "revision": "c9b31f27ce0803bcbda04bfd0eba249e",
+    "url": "/pampa-three/static/js/2.b0c358bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b3403c9d39eaae7b7fe",
+    "url": "/pampa-three/static/js/main.b08e3625.chunk.js"
+  },
+  {
+    "revision": "7617d3bc19e17f3d2275",
+    "url": "/pampa-three/static/js/runtime-main.88687fd5.js"
+  }
+]);
