dd4d35de59ee71b7cfb3220f1aefa22158bd5a0f
Alejandro de Frutos <adefrutoscasado@gmail.com>
2021-01-24 17:28:56 +0100

    Prettier format code

--- src/Candle.js
+++ src/Candle.js
@@ -1,118 +1,109 @@
 import * as THREE from 'three'
 import React, { useMemo } from 'react'
 import { useFrame, useThree } from 'react-three-fiber'
-import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
-import { SubdivisionModifier } from 'three/examples/jsm/modifiers/SubdivisionModifier';
-import './styles.css'
+import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader'
+import { SubdivisionModifier } from 'three/examples/jsm/modifiers/SubdivisionModifier'
 
 function getCenterPoint(mesh) {
-    const geometry = mesh.geometry
-    geometry.computeBoundingBox()
-    const center = new THREE.Vector3()
-    geometry.boundingBox.getCenter(center)
-    mesh.localToWorld(center)
-    return center
+  const geometry = mesh.geometry
+  geometry.computeBoundingBox()
+  const center = new THREE.Vector3()
+  geometry.boundingBox.getCenter(center)
+  mesh.localToWorld(center)
+  return center
 }
 
 /**
  * Generates a smooth geometry from a shaded one
  * https://threejs.org/examples/?q=subd#webgl_modifier_subdivision
  */
-function subdivide( geometry, subdivisions ) {
-    const faceIndices = ['a','b','c']
-    const modifier = new SubdivisionModifier(subdivisions)
-    const smoothGeometry = modifier.modify(geometry)
-    // colorify faces
-    for ( let i = 0; i < smoothGeometry.faces.length; i ++ ) {
-        const face = smoothGeometry.faces[ i ];
-        for ( let j = 0; j < 3; j ++ ) {
-
-            const vertexIndex = face[faceIndices[j]]
-            const vertex = smoothGeometry.vertices[ vertexIndex ]
-
-            const hue = (vertex.y/200) + 0.5;
-            const color = new THREE.Color().setHSL(hue,1,0.5)
-            face.vertexColors[j] = color
-        }
+function subdivide(geometry, subdivisions) {
+  const faceIndices = ['a', 'b', 'c']
+  const modifier = new SubdivisionModifier(subdivisions)
+  const smoothGeometry = modifier.modify(geometry)
+  // colorify faces
+  for (let i = 0; i < smoothGeometry.faces.length; i++) {
+    const face = smoothGeometry.faces[i]
+    for (let j = 0; j < 3; j++) {
+      const vertexIndex = face[faceIndices[j]]
+      const vertex = smoothGeometry.vertices[vertexIndex]
+      const hue = vertex.y / 200 + 0.5
+      const color = new THREE.Color().setHSL(hue, 1, 0.5)
+      face.vertexColors[j] = color
     }
-    // convert to THREE.BufferGeometry
-    return new THREE.BufferGeometry().fromGeometry(smoothGeometry)
+  }
+  // convert to THREE.BufferGeometry
+  return new THREE.BufferGeometry().fromGeometry(smoothGeometry)
 }
-
 
 function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
+  return Math.floor(Math.random() * (max - min)) + min
 }
 
-
 export default function Candle({ stringifiedSrc, material, test, ...props }) {
-    const { camera } = useThree()
+  const { camera } = useThree()
 
-    const object = useMemo(() => {
-        const loader = new OBJLoader()
-        return loader.parse(stringifiedSrc)
-    }, [])
+  const object = useMemo(() => {
+    const loader = new OBJLoader()
+    return loader.parse(stringifiedSrc)
+  }, [])
 
-    const candleMaterial = useMemo(() => {
-        return material || new THREE.MeshNormalMaterial()
-    }, [])
+  const candleMaterial = useMemo(() => {
+    return material || new THREE.MeshNormalMaterial()
+  }, [])
 
-    const theta = useMemo(() => {
-        const randomTheta = () => getRandomInt(3, 7)
-        return {
-            x: randomTheta(),
-            y: randomTheta(),
-            z: randomTheta()
-        }
-    }, [])
+  const theta = useMemo(() => {
+    const randomTheta = () => getRandomInt(3, 7)
+    return {
+      x: randomTheta(),
+      y: randomTheta(),
+      z: randomTheta()
+    }
+  }, [])
 
-    useMemo(() => {
-        object.traverse(function (child) {
-            if (child instanceof THREE.Mesh) {
-                child.material = candleMaterial
-                const MODIFIER_SUBDIVISIONS = 2
-                child.geometry = subdivide(child.geometry, MODIFIER_SUBDIVISIONS)
-            }
-        })
-    }, [object])
-
-    useFrame(() => {
-        if (object && object.children && object.children[0]) {
-            const center = getCenterPoint(object.children[0])
-            camera.lookAt(center)
-            camera.updateProjectionMatrix()
-        }
+  useMemo(() => {
+    object.traverse(function (child) {
+      if (child instanceof THREE.Mesh) {
+        child.material = candleMaterial
+        const MODIFIER_SUBDIVISIONS = 2
+        child.geometry = subdivide(child.geometry, MODIFIER_SUBDIVISIONS)
+      }
     })
+  }, [object])
 
-    useFrame(() => {
-        if (object) {
-            const timer = Date.now() * 0.00025
-            object.position.x = Math.sin(timer * theta.x) * 10
-            object.position.y = Math.cos(timer * theta.y) * 10
-            object.position.z = Math.cos(timer * theta.z) * 10
-        }
-    })
+  useFrame(() => {
+    if (object && object.children && object.children[0]) {
+      const center = getCenterPoint(object.children[0])
+      camera.lookAt(center)
+      camera.updateProjectionMatrix()
+    }
+  })
 
-    useFrame(() => {
-        if (object) {
-            const timer = Date.now() * 0.00025
-            object.rotateX(Math.sin(timer * theta.x) / 1000)
-            object.rotateY(Math.cos(timer * theta.y) / 1000)
-            object.rotateZ(Math.cos(timer * theta.z) / 1000)
-        }
-    })
+  useFrame(() => {
+    if (object) {
+      const timer = Date.now() * 0.00025
+      object.position.x = Math.sin(timer * theta.x) * 10
+      object.position.y = Math.cos(timer * theta.y) * 10
+      object.position.z = Math.cos(timer * theta.z) * 10
+    }
+  })
 
-    return (
-        <>
-            {object &&
-                <mesh
-                    scale={[0.01, 0.01, 0.01]}
-                    position={[0, -1.5, 0]}
-                    {...props}
-                >
-                    <primitive attach="mesh" object={object} />
-                </mesh>
-            }
-        </>
-    )
+  useFrame(() => {
+    if (object) {
+      const timer = Date.now() * 0.00025
+      object.rotateX(Math.sin(timer * theta.x) / 1000)
+      object.rotateY(Math.cos(timer * theta.y) / 1000)
+      object.rotateZ(Math.cos(timer * theta.z) / 1000)
+    }
+  })
+
+  return (
+    <>
+      {object && (
+        <mesh scale={[0.01, 0.01, 0.01]} position={[0, -1.5, 0]} {...props}>
+          <primitive attach="mesh" object={object} />
+        </mesh>
+      )}
+    </>
+  )
 }
--- src/index.js
+++ src/index.js
@@ -6,29 +6,22 @@ import { OrbitControls } from 'drei'
 import queryString from 'query-string'
 import './styles.css'
 
-const {query} = queryString.parseUrl(window.location.href)
+const { query } = queryString.parseUrl(window.location.href)
 
 const containsQueryString = (string) => {
-  return Object.keys(query).findIndex(
-    v => v.toUpperCase() === string.toUpperCase()
-  ) > -1
+  return Object.keys(query).findIndex((v) => v.toUpperCase() === string.toUpperCase()) > -1
 }
-const queryStringColor = function(){
+const queryStringColor = (function () {
   if (!query.color) return null
-  return query.color.startsWith('#') ?
-    query.color
-    :
-    '#' + query.color
-}()
+  return query.color.startsWith('#') ? query.color : '#' + query.color
+})()
 
 const CandleA = lazy(() => import('./models/candleA'))
 const CandleB = lazy(() => import('./models/candleB'))
 const CandleC = lazy(() => import('./models/candleC'))
 
 const Scene = () => {
-  const {
-    camera
-  } = useThree()
+  const { camera } = useThree()
 
   useFrame(() => {
     const MAX_ZOOM = 300
@@ -46,21 +39,21 @@ const Scene = () => {
       <ambientLight />
       <spotLight position={[10, 10, 10]} />
       <pointLight position={[-10, -10, -10]} color="orange" />
-      {containsQueryString('candleA') &&
+      {containsQueryString('candleA') && (
         <Suspense fallback={null}>
-            <CandleA material={candleMaterial} />
+          <CandleA material={candleMaterial} />
         </Suspense>
-      }
-      {containsQueryString('candleB') &&
+      )}
+      {containsQueryString('candleB') && (
         <Suspense fallback={null}>
-            <CandleB material={candleMaterial} />
+          <CandleB material={candleMaterial} />
         </Suspense>
-      }
-      {containsQueryString('candleC') &&
+      )}
+      {containsQueryString('candleC') && (
         <Suspense fallback={null}>
-            <CandleC material={candleMaterial} />
+          <CandleC material={candleMaterial} />
         </Suspense>
-      }
+      )}
       <OrbitControls />
     </>
   )
@@ -70,8 +63,9 @@ ReactDOM.render(
   <Canvas
     colorManagement
     orthographic
-    camera={{ position: [0, 2, 5], zoom: 200, fov: 50 }}
-    pixelRatio={window.devicePixelRatio}
+    camera={{ position: [0, 2, 5],
+    zoom: 200, fov: 50 }}
+    pixelRatio={window.devicePixelRatio || 1}
   >
     <Scene />
   </Canvas>,
