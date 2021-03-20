ec256086aec3db69cce1b4259c8cd9d7786280ed
Alejandro de Frutos <adefrutos-e@sice-outsourcing.com>
2021-03-20 12:21:37 +0100

    Use gltf format instead of obj

---
+++ scripts/objToGltf.js
@@ -0,0 +1,25 @@
+const obj2gltf = require('obj2gltf')
+const fs = require('fs')
+
+const filenames = [
+    'candleA', 
+    'candleB', 
+    'candleC', 
+    'candleD', 
+    'candleE', 
+    'candleF', 
+]
+
+const convertToGltf = async (filename) => {
+    const gltf = await obj2gltf(`./objModels/${filename}.obj`)
+    const data = Buffer.from(JSON.stringify(gltf))
+    fs.writeFileSync(`./public/${filename}.gltf`, data)
+}
+
+
+const main = async () => {
+    const conversions = filenames.map(convertToGltf)
+    await Promise.all(conversions)
+}
+
+main()
